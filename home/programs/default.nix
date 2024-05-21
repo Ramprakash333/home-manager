4d9af0b98d08221f643175b618dea57ee6c8085b
Ramprakash333 <ramprakashvel@gmail.com>
2024-05-22 03:53:29 +0530

    added lazy git

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -7,7 +7,7 @@
         ./starShip.nix
         ./zsh.nix
     ];
-     nix.gc = {
+    nix.gc = {
         automatic = true;
         frequency = "weekly";
         options = "--delete-older-than 1w";
